13d555b9167bdd3662efa9779c624ffd0bc78c7a
vrajSpatel <VSPATEL012004@GMAIL.COM>
2024-10-28 18:52:44 +0530

    api

--- src/app/Component/Header.js
+++ src/app/Component/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
                     <div className="flex items-center space-x-4">
                         {/* <!-- Logo --> */}
                         <Link href="/" className="text-2xl font-bold text-gray-800">
-                            Logo
+                            BArk
                         </Link>
 
                         {/* <!-- Dropdown --> */}
---
+++ src/app/api/api.js
@@ -0,0 +1,320 @@
+export default{
+  "field": {
+    "household": [
+      {
+        "name": "Gardening",
+        "image": "",
+        "online": true
+      },
+      {
+        "name": "House Cleaning",
+        "image": "",
+        "online": false
+      },
+      {
+        "name": "Painting Service",
+        "image": "",
+        "online": false
+      },
+      {
+        "name": "Arctitectural Service",
+        "image": "",
+        "online": false
+      },
+      {
+        "name": "Arctitectural Service",
+        "image": "",
+        "online": true
+      },
+      {
+        "name": "Building Contractors",
+        "image": "",
+        "online": false
+      },
+      {
+        "name": "CCTV Installation",
+        "image": "",
+        "online": false
+      },
+      {
+        "name": "Interior Designer",
+        "image": "",
+        "online": true
+      },
+      {
+        "name": "Deep Cleaning",
+        "image": "",
+        "online": false
+      },
+      {
+        "name": "House Cleaning",
+        "image": "",
+        "online": false
+      },
+      {
+        "name": "Pest Control",
+        "image": "",
+        "online": false
+      },
+      {
+        "name": "SolarPanel Installation",
+        "image": "",
+        "online": true
+      }
+    ]
+  },
+
+  "Health": [
+    {
+      "name": "personal trainer",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "counselling",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Massage",
+      "image": "",
+      "online": false
+    },
+    {
+      "name": "Acupuncture & Acupressure",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": " Addiction Therapy",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Adolescent Counselling",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Hair Loss Treatment",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Life Coaching",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Massage Therapy",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Private Doctors",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Therapist",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Psychotherapy",
+      "image": "",
+      "online": true
+    }
+  ],
+
+  "Sports and Fitness": [
+    {
+      "name": "Badminton Lessons",
+      "image": "",
+      "online": false
+    },
+    {
+      "name": "Baseball Lessons",
+      "image": "",
+      "online": false
+    },
+    {
+      "name": "Basketball Lessons",
+      "image": "",
+      "online": false
+    },
+    {
+      "name": "Gymnastics Classes",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Personal Trainers",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Surfing Lessons",
+      "image": "",
+      "online": false
+    },
+    {
+      "name": "Table Tennis Coaching",
+      "image": "",
+      "online": false
+    },
+    {
+      "name": "Yoga Classes",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Zumba Classes",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Ice Skating Lessons",
+      "image": "",
+      "online": false
+    },
+    {
+      "name": "Martial Arts Classes",
+      "image": "",
+      "online": true
+    }
+  ],
+  "Entertainment": {
+    "Children": [
+      {
+        "name": "Balloon Twisters & Entertainers",
+        "image": "",
+        "online": false
+      },
+      {
+        "name": "Bouncy Castle Hire",
+        "image": "",
+        "online": true
+      },
+      {
+        "name": "Clown Entertainment",
+        "image": "",
+        "online": true
+      },
+      {
+        "name": "Face Painting",
+        "image": "",
+        "online": true
+      },
+      {
+        "name": "Magician",
+        "image": "",
+        "online": true
+      },
+      {
+        "name": "Puppet Show Entertainment",
+        "image": "",
+        "online": false
+      }
+    ],
+
+    "For All": [
+      {
+        "name": "Animal Show Entertainment",
+        "image": "",
+        "online": false
+      },
+      {
+        "name": "Balloon Artistry",
+        "image": "",
+        "online": false
+      },
+      {
+        "name": "Balloon Twisters & Entertainers",
+        "image": "",
+        "online": false
+      },
+      {
+        "name": "Belly Dancing",
+        "image": "",
+        "online": true
+      },
+      {
+        "name": "Circus Acting",
+        "image": "",
+        "online": false
+      },
+      {
+        "name": "Magician",
+        "image": "",
+        "online": true
+      },
+      {
+        "name": "Miming",
+        "image": "",
+        "online": true
+      },
+      {
+        "name": "Storytellers",
+        "image": "",
+        "online": true
+      },
+      {
+        "name": "Dance Entertainment",
+        "image": "",
+        "online": false
+      },
+      {
+        "name": "Puppet Show Entertainment",
+        "image": "",
+        "online": true
+      },
+      {
+        "name": "Temporary Tattoo Artistry",
+        "image": "",
+        "online": false
+      }
+    ]
+  },
+
+  "Food Service": [
+    {
+      "name": "Bartending",
+      "image": "",
+      "online": false
+    },
+    {
+      "name": "Cake Making",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Candy Buffet Services",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Catering",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Wedding Cakes",
+      "image": "",
+      "online": true
+    },
+    {
+      "name": "Food Truck Catering",
+      "image": "",
+      "online": false
+    }
+  ],
+
+  "Music": [],
+
+  "Production": [],
+
+  "Wedding": [],
+
+  "Bussniess": []
+}
