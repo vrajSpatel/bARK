05a092481f6935a92c3399be72384a20dbc138dd
Dharmikpate120 <Dharmik7458@gmail.com>
2024-10-28 19:34:52 +0530

    fixing api file

--- src/app/explore/page.js
+++ src/app/explore/page.js
@@ -1,54 +1,58 @@
-import "../explore/explore.css"
-import Feild from '../api/api'
+import "../explore/explore.css";
+import field from "../api/api.js";
 
 const explore = () => {
-    // const jstu = JSON.stringify(Feild)
-    // console.log(jstu.household)
-    console.log()
-    return (
-        <>
-            <div className="homemain">
-                <div className="introtext">
-                    <h1>Find Best In <br /> Every Profession </h1>
-                </div>
+  //   console.log(field);
+  var field1 = JSON.parse(field);
+  console.log(field1.field.household);
+  // const jstu = JSON.stringify(Feild)
+  // console.log(jstu.household)
+  console.log();
+  return (
+    <>
+      <div className="homemain">
+        <div className="introtext">
+          <h1>
+            Find Best In <br /> Every Profession{" "}
+          </h1>
+        </div>
 
-                <div className="searchbars">
-                    <div className="inputsearch">
-                        <input type="text" id="service" placeholder="what service you looking for? " />
-                        <input type="text" id="pincode" placeholder="Pincode" />
-                    </div>
-                    <div className="searchbutton">
-                        Search
-                    </div>
+        <div className="searchbars">
+          <div className="inputsearch">
+            <input
+              type="text"
+              id="service"
+              placeholder="what service you looking for? "
+            />
+            <input type="text" id="pincode" placeholder="Pincode" />
+          </div>
+          <div className="searchbutton">Search</div>
+        </div>
 
-                </div>
+        <div className="optionavailable">
+          <h1 className="professions">Professions</h1>
+          <h3>( that are mostly trendy )</h3>
 
-                <div className="optionavailable">
-                    <h1 className="professions">Professions</h1>
-                    <h3>( that are mostly trendy )</h3>
-
-                    <div className="options">
-                        <div className="container_option">
-                            <div className="op">household</div>
-                            <div className="op">Health</div>
-                            <div className="op">Sports</div>
-                            <div className="op">Fitness</div>
-                            <div className="op">Entertainment</div>
-                            <div className="op">Food Service</div>
-                            <div className="op">Music</div>
-                            <div className="op">Production</div>
-                            <div className="op">Wedding</div>
-                            <div className="op">Bussniess</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="subselection">
-                    
-                </div>
+          <div className="options">
+            <div className="container_option">
+              <div className="op">household</div>
+              <div className="op">Health</div>
+              <div className="op">Sports</div>
+              <div className="op">Fitness</div>
+              <div className="op">Entertainment</div>
+              <div className="op">Food Service</div>
+              <div className="op">Music</div>
+              <div className="op">Production</div>
+              <div className="op">Wedding</div>
+              <div className="op">Bussniess</div>
             </div>
-        </>
-    )
-}
+          </div>
+        </div>
 
-export default explore
+        <div className="subselection"></div>
+      </div>
+    </>
+  );
+};
+
+export default explore;
